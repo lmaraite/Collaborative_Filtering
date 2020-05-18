be621ebdf3f8d54addff27349c1fa1c492f65049
leonm13 <leon.maraite@gmail.com>
2020-05-18 15:10:41 +0200

    Implemented prediction

--- neighborhood.py
+++ neighborhood.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
-
-similarity_matrix = np.genfromtxt("small-dataset/cosine-similarity-matrix.csv", delimiter=",")
-rating_matrix = np.genfromtxt("small-dataset/R.csv", delimiter=",")
-is_rated_matrix = np.genfromtxt("small-dataset/Y.csv", delimiter=",")
+from data import dataset
 
 """
 Item based:
@@ -15,21 +12,16 @@ User based:
     element_id = movie_id
 """
 
+
 class neighbor:
-    def __init__(self, rating: int, key_id: int, similarity: float):
+    def __init__(self, rating: int, key_id: int, similarity: float, pearson_average: float):
         self.rating = rating
         self.key_id = key_id
         self.similarity = similarity
+        self.pearson_average = pearson_average
 
     def __lt__(self, neighbor2):
         return self.similarity < neighbor2.similarity
-
-
-class dataset:
-    def __init__(self, similarity_matrix: np.ndarray, rating_matrix: np.ndarray, is_rated_matrix: np.ndarray):
-        self.similarity_matrix = similarity_matrix
-        self.rating_matrix = rating_matrix
-        self.is_rated_matrix = is_rated_matrix
 
 
 def has_rated(key_id: int, element_id: int, is_rated_matrix: np.ndarray) -> bool:
@@ -42,8 +34,8 @@ def get_neighbors(key_id: int, element_id: int, data: dataset) -> list:
         if has_rated(it_key_id, element_id, data.is_rated_matrix):
             neighbors.append(neighbor(data.rating_matrix[it_key_id][element_id]
                             , it_key_id
-                            , data.similarity_matrix[key_id][it_key_id]))
-    return neighbors
+                            , data.similarity_matrix[key_id][it_key_id], 0))  # pearson_average wird standartmäßig auf
+    return neighbors                                                          # 0 gesetzt und später in der prediction initialisiert
 
 
 def get_nearest_neighbors(max_nearest_neighbors: int , key_id: int, element_id: int, data: dataset) -> list:
@@ -52,13 +44,3 @@ def get_nearest_neighbors(max_nearest_neighbors: int , key_id: int, element_id: 
         return neighbors
     else:
         return neighbors[:max_nearest_neighbors]
-
-
-
-
-test_data = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
-for e in get_nearest_neighbors(max_nearest_neighbors=5, key_id=4, element_id=0, data=test_data):
-    print("----------------")
-    print(e.rating)
-    print(e.key_id)
-    print(e.similarity)
--- prediction.py
+++ prediction.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python3
+from data import dataset
+from neighborhood import get_nearest_neighbors
+import numpy as np
+
+"""
+Item based:
+    key_id     = movie_id
+    element_id = user_id
+
+User based:
+    key_id     = user_id
+    element_id = movie_id
+"""
+
+MAX_NEAREST_NEIGHBORS = 3
+
+
+similarity_matrix = np.genfromtxt("small-dataset/cosine-similarity-matrix.csv", delimiter=",")
+rating_matrix = np.genfromtxt("small-dataset/R.csv", delimiter=",")
+is_rated_matrix = np.genfromtxt("small-dataset/Y.csv", delimiter=",").astype(bool)
+
+
+def predicition_cosine_similarity(key_id: int, element_id: int, data: dataset) -> float:
+    nearest_neighbors = get_nearest_neighbors(MAX_NEAREST_NEIGHBORS, key_id, element_id, data)
+    counter = 0
+    denominator = 0
+    for it in nearest_neighbors:
+        counter += it.similarity * it.rating
+        denominator += it.similarity
+    return counter / denominator
+
+
+def add_pearson_average(key_id: int, nearest_neighbors: list, data: dataset):
+    for it in nearest_neighbors:
+        rating_vector = data.rating_matrix[it.key_id]
+        intersection = np.logical_and(data.is_rated_matrix[key_id], data.is_rated_matrix[it.key_id])
+        intersected_rating_vector = rating_vector[intersection]
+        it.pearson_average = np.average(intersected_rating_vector)
+
+
+def predicition_pearson_correlation(key_id: int, element_id: int, data: dataset) -> float:
+    nearest_neighbors = get_nearest_neighbors(MAX_NEAREST_NEIGHBORS, key_id, element_id, data)
+    add_pearson_average(key_id, nearest_neighbors, data)
+    counter = 0
+    denominator = 0
+    for it in nearest_neighbors:
+        counter += it.similarity * (it.rating - it.pearson_average)
+        denominator += it.similarity
+    return counter / denominator
+
+
+test_data = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
+print(predicition_pearson_correlation(4, 0, test_data))
