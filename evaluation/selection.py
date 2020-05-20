b2050f75b90907efb9f282cd9737ee5139affe63
Claas Vonderschen <claas.vonderschen@adesso.de>
2020-05-20 21:39:37 +0200

    Fix: filter possible indices by is_rated matrix in hold_out

--- evaluation/selection.py
+++ evaluation/selection.py
@@ -2,15 +2,15 @@ import numpy as np
 
 import numpy.random
 
-def select_indices_with_hold_out(shape, train_size=0.8) -> np.array:
+def select_indices_with_hold_out(shape, is_rated, train_size=0.8) -> np.array:
     indices = np.array(
         [(x, y) for x in range(shape[0])
                     for y in range(shape[1])]
     )
-    size = shape[0] * shape[1]
-
+    indices = indices[is_rated.flat]
     random_generator = numpy.random.default_rng()
 
+    size = indices.shape[0]
     max_number_elements = round(size * train_size)
     return random_generator.permutation(indices)[:max_number_elements]
 
--- test/evaluation/selection_test.py
+++ test/evaluation/selection_test.py
@@ -6,7 +6,7 @@ import numpy.random
 
 from evaluation import selection
 
-class HoldOutTest(unittest.TestCase):
+class SelectionTest(unittest.TestCase):
 
     @pytest.fixture(autouse=True)
     def setup(self, mocker):
@@ -15,8 +15,13 @@ class HoldOutTest(unittest.TestCase):
     def test_hold_out_with_train_size_05(self):
         #given
         shape = (4, 3)
+        is_rated = np.array([True for x in range(12)]).reshape(shape)
         #when
-        return_indices = selection.select_indices_with_hold_out(shape, train_size=0.5)
+        return_indices = selection.select_indices_with_hold_out(
+            shape,
+            is_rated,
+            train_size=0.5
+        )
         #then
         expected_indices = np.array([
             [0, 0],
@@ -27,4 +32,33 @@ class HoldOutTest(unittest.TestCase):
             [1, 0],
           ])
 
+        assert return_indices.shape == expected_indices.shape
         assert (return_indices == expected_indices).all()
+
+    def test_hold_out_with_is_rated_matrix(self):
+        #given
+        shape = (4, 3)
+        is_rated = np.array([
+            [True, True, False],
+            [True, False, True],
+            [False, True, False],
+            [True, True, False]
+        ])
+
+        #then
+        return_indices = selection.select_indices_with_hold_out(
+            shape,
+            is_rated,
+            train_size=0.5
+        )
+        #then
+        expected_indices = np.array([
+            (1, 2),
+            (1, 0),
+            (3, 1),
+            (2, 1)
+        ])
+
+        assert return_indices.shape == expected_indices.shape
+        assert (return_indices == expected_indices).all()
+
