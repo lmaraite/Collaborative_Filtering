3c09206c5f1df61e888ca1617de47ca81744a13d
dkettner <kettner_dennis@web.de>
2020-05-20 22:14:07 +0200

    Fix: accuracy improvements for adjusted_cosine Wrong adjustment of co-ratings in convert_to_adjusted_cosine got corrected. New function: get_actual_element_ratings New function: get_is_co_rated Deleted function: get_column_specific_means
    Major speed improvements:
    New and faster implementation of get_co_ratings.
    
    Other changes:
    Applied functions of new input module for loading the data.
    Removed rounding from get_similarity.

--- collaborative_filtering/similarity.py
+++ collaborative_filtering/similarity.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
 import numpy as np
-from numpy.linalg import norm
 import itertools
+from numpy.linalg import norm
 from progressbar import print_progressbar
 
+# TODO: Ignore RuntimeWarning caused by division by zero.
+# TODO: It's possible to save computation time for adjusted_cosine by computing and saving all element-means in advance.
+# TODO: All cases that produce a nan in get_similarity will still be computed twice.
+# TODO: Change from vertical to horizontal alignment of co_ratings. Affects multiple functions.
+# TODO: Progressbar is not working reliably since the new implementation of get_co_ratings.
+# TODO: Find faster solution for numpy iteration with two-dimensional indexing.
+# TODO: Should the results of compute_similarity be rounded?
 # TODO: Implement further functions to tidy up the code.
-# TODO: Search for the problems causing the RuntimeWarning when computing item-based adjusted cosine.
-# TODO: Change comment style to javadoc to document all parameters etc.
+# TODO: Use pydoc for documentation.
 
 '''
     Creates a similarity-matrix by iterating over all_ratings and saving every similarity at both
@@ -31,29 +36,38 @@ def create_similarity_matrix(all_ratings, is_rated, mode):
     co_ratings to prevent computations errors.
     Returns the computed similarity or nan if there are less than two co_ratings.
 '''
-def get_similarity(elements_ids, all_ratings, is_rated, mode):
-    co_ratings = get_co_ratings(elements_ids, all_ratings, is_rated)
+def get_similarity(element_ids, all_ratings, is_rated, mode):
+    co_ratings = get_co_ratings(element_ids, all_ratings, is_rated)
     if less_than_two_co_ratings(co_ratings):
         return np.nan
     else:
-        adjusted_co_ratings = get_adjusted_co_ratings(elements_ids, all_ratings, co_ratings, mode)
+        adjusted_co_ratings = get_adjusted_co_ratings(element_ids, all_ratings, is_rated, co_ratings, mode)
         return compute_similarity(adjusted_co_ratings)
 
 
 '''
-    Collects all co_ratings by iterating over two rating-arrays and two is_rated-arrays 
-    at the same time. If two users rated the same item the co_rating will be added.
+    Collects all co_ratings by masking all_ratings with a boolean is_co_rated array.
+    If two users rated the same item the co_rating will be added.
     Returns the compiled array of co_ratings.
 '''
-# TODO: It's possible to omit is_rated by checking all_ratings for zeroes.
-# TODO: Replace .append() with a faster solution.
-def get_co_ratings(elements_ids, all_ratings, is_rated):
-    co_ratings = np.empty(shape=[0, 2])
-    for element1_rating, element2_rating, element1_is_rated, element2_is_rated \
-            in zip(all_ratings[elements_ids[0], :], all_ratings[elements_ids[1], :], is_rated[elements_ids[0], :], is_rated[elements_ids[1], :]):
-        if element1_is_rated and element2_is_rated:
-            co_ratings = np.append(co_ratings, [[element1_rating, element2_rating]], axis=0)
-    return co_ratings
+# TODO: Find a way to index both rows at the same time to avoid vstack.
+def get_co_ratings(element_ids, all_ratings, is_rated):
+    is_co_rated = get_is_co_rated(element_ids, is_rated)
+
+    ratings_element1 = all_ratings[element_ids[0], :]
+    ratings_element2 = all_ratings[element_ids[1], :]
+    co_ratings = np.vstack((ratings_element1[is_co_rated], ratings_element2[is_co_rated]))
+
+    # Transposing to get vertical alignment for further computations.
+    return co_ratings.T
+
+
+'''
+    Uses a logical AND to produce a boolean array that can be used as a mask on the original ratings matrix.
+    Returns boolean information about whether the given pair of elements share ratings by individual users.
+'''
+def get_is_co_rated(element_ids, is_rated):
+    return np.logical_and(is_rated[element_ids[0], :], is_rated[element_ids[1], :])
 
 
 '''
@@ -69,12 +83,12 @@ def less_than_two_co_ratings(co_ratings):
     cosine. Defaults to cosine if the mode is unknown. 
     Returns the algorithm-adjusted co_ratings.
 '''
-def get_adjusted_co_ratings(elements_ids, all_ratings, co_ratings, mode):
-    if (mode == "adjusted_cosine"):
-        return convert_to_adjusted_cosine(elements_ids, all_ratings, co_ratings)
-    elif (mode == "pearson"):
+def get_adjusted_co_ratings(element_ids, all_ratings, is_rated, co_ratings, mode):
+    if mode == "adjusted_cosine":
+        return convert_to_adjusted_cosine(element_ids, all_ratings, is_rated, co_ratings)
+    elif mode == "pearson":
         return convert_to_pearson(co_ratings)
-    elif (mode == "cosine"):
+    elif mode == "cosine":
         return co_ratings
     else:
         print("Unknown mode selected. The algorithm defaults to cosine similarity.")
@@ -86,42 +100,37 @@ def get_adjusted_co_ratings(elements_ids, all_ratings, co_ratings, mode):
     Returns the adjusted co_ratings.
 '''
 def convert_to_pearson(co_ratings):
-    return co_ratings - get_column_specific_means(co_ratings)
+    return co_ratings - np.array([np.mean(co_ratings[:, 0]), np.mean(co_ratings[:, 1])])
 
 
 '''
-    Adjusts the co_ratings to adjusted_cosine by subtracting the means of all ratings given to the specific elements.
-    Extra steps are needed to filter only the element-specific ratings from all_ratings.
+    Adjusts the co_ratings to adjusted_cosine by subtracting the means of all ratings 
+    given to the specific elements. Extra steps are needed to filter only the element-specific 
+    ratings from all_ratings considering that some may be redundant.
     Returns the adjusted co_ratings.
 '''
-# TODO: Abstract an implementation of get_element_filtered_ratings from this function.
-# TODO: It's possible to save a lot computation time by computing and saving all element-specific means in advance.
-def convert_to_adjusted_cosine(elements_ids, all_ratings, co_ratings):
-    all_ratings[all_ratings == 0] = np.nan
-    element_filtered_ratings = np.vstack([all_ratings[elements_ids[0], :], all_ratings[elements_ids[1], :]])
+def convert_to_adjusted_cosine(element_ids, all_ratings, is_rated, co_ratings):
+    actual_ratings_element1 = get_actual_element_ratings(element_ids[0], all_ratings, is_rated)
+    actual_ratings_element2 = get_actual_element_ratings(element_ids[1], all_ratings, is_rated)
 
-    # Transposing in order to use get_column_specific_means().
-    return co_ratings - get_column_specific_means(element_filtered_ratings.transpose())
+    # Subtracts the means of all element-specific ratings column-wise.
+    return co_ratings - np.array([np.mean(actual_ratings_element1), np.mean(actual_ratings_element2)])
 
 
 '''
-    Computes the means of all elements column-wise. All nan-values are ignored to prevent wrong means.
-    Returns an array with two values: One mean for each column.
+    Filters all 'ratings' which are actually redundant.
+    Returns an array with only the element's real ratings.
 '''
-def get_column_specific_means(ratings):
-    return np.array([np.nanmean(ratings[:, 0]), np.nanmean(ratings[:, 1])])
+def get_actual_element_ratings(element_id, all_ratings, is_rated):
+    element_ratings = all_ratings[element_id, :]
+    return element_ratings[is_rated[element_id, :]]
 
 
 '''
     The final step to compute the similarity between two elements according to (Jannach et. al, 2011, p.19). 
-    Small internal rounding errors unveil if an element is compared to itself which will not produce a
-    similarity of 1.0 . An additional rounding helps to correct these instances. 
-    Will produce a RuntimeWarning if a division by zero is attempted.
+    Small internal rounding errors unveil when an element is compared to itself which will not return a
+    similarity of 1.0 . Will produce a RuntimeWarning if a division by zero is attempted.
     Returns the resulting similarity.
 '''
 def compute_similarity(ratings):
-    similarity = np.dot(ratings[:, 0], ratings[:, 1]) / (norm(ratings[:, 0]) * norm(ratings[:, 1]))
-
-    # Rounding to accommodate previous rounding errors due to floating-point arithmetic.
-    return round(similarity, 14)
-
+    return np.dot(ratings[:, 0], ratings[:, 1]) / (norm(ratings[:, 0]) * norm(ratings[:, 1]))
