e471171097ebf2358ecaade50e161f6accb87f2d
Claas Vonderschen <claas.vonderschen@smail.th-koeln.de>
2020-05-21 02:12:57 +0200

    Add: AccurancyEvaluationProperties

--- collaborative_filtering/evaluation/accurancy.py
+++ collaborative_filtering/evaluation/accurancy.py
@@ -1,6 +1,51 @@
 import numpy as np
 import math
 
+from evaluation import EvaluationProperties, EvaluationPropertiesBuilder
+
+class AccurancyEvaluationProperties(EvaluationProperties):
+
+    def __init__(
+        self,
+        ratings_matrix: np.ndarray,
+        is_rated_matrix: np.ndarray,
+        similarity: str,
+        selection_strategy,
+        error_measurement
+    ):
+        super().__init__(
+            ratings_matrix,
+            is_rated_matrix,
+            similarity,
+            selection_strategy
+        )
+        self.error_measurement = error_measurement
+
+class AccurancyEvaluationPropertiesBuilder(EvaluationPropertiesBuilder):
+
+    def __init__(self):
+        super().__init__()
+        self.error_measurement = None
+
+    def with_error_measurement(self, error_measurement):
+        self.error_measurement = error_measurement
+        return self
+
+    def build(self):
+        if not self._are_properties_complete():
+            raise ValueError("Initialization not complete")
+
+        return AccurancyEvaluationProperties(
+            self.ratings_matrix,
+            self.is_rated_matrix,
+            self.similarity,
+            self.selection_strategy,
+            self.error_measurement
+        )
+
+    def _are_properties_complete(self):
+        return super()._are_properties_complete() and not self.error_measurement is None
+
 def error(prediction, rating):
     return prediction - rating
 
--- test/collaborative_filtering/evaluation/accurancy_test.py
+++ test/collaborative_filtering/evaluation/accurancy_test.py
@@ -1,8 +1,10 @@
 import unittest
 import numpy as np
 import math
+import pytest
 
 from evaluation import accurancy as ac
+from evaluation.accurancy import AccurancyEvaluationPropertiesBuilder
 
 class ErrorTest(unittest.TestCase):
 
@@ -41,3 +43,44 @@ class RootMeanSquaredErrorTest(unittest.TestCase):
 
         #then
         assert math.isclose(error, expected_error)
+
+class AccurancyEvaluationPropertiesBuilderTest(unittest.TestCase):
+
+    dummy_function = lambda x, y, z: x
+
+    def test_builder_should_throw_error_without_error_measurement(self):
+        with pytest.raises(ValueError):
+            AccurancyEvaluationPropertiesBuilder() \
+                .with_is_rated_matrix(np.array([])) \
+                .with_ratings_matrix(np.array([])) \
+                .with_similarity("test") \
+                .with_selection_strategy(self.dummy_function) \
+                .build()
+
+    def test_builder(self):
+        #given
+        ratings_matrix = np.array([
+            [3, 1, 2],
+            [5, 2, 4],
+            [1, 2, 4]
+        ])
+        is_rated = np.array([
+            [True, True, False],
+            [True, False, True],
+            [False, True, False]
+        ])
+        #when
+        evaluation_properties = AccurancyEvaluationPropertiesBuilder() \
+            .with_ratings_matrix(ratings_matrix) \
+            .with_is_rated_matrix(is_rated) \
+            .with_similarity("testSimilarity") \
+            .with_selection_strategy(self.dummy_function) \
+            .with_error_measurement(self.dummy_function) \
+            .build()
+
+        #then
+        assert (evaluation_properties.ratings_matrix == ratings_matrix).all()
+        assert (evaluation_properties.is_rated_matrix == is_rated).all()
+        assert (evaluation_properties.similarity == "testSimilarity")
+        assert (evaluation_properties.selection_strategy == self.dummy_function)
+        assert (evaluation_properties.error_measurement == self.dummy_function)
