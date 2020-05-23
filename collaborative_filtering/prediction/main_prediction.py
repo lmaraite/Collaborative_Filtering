d14d0046aaa68f9ea2352625678f3c3590789430
lmaraite <leon.maraite@gmail.com>
2020-05-23 17:39:06 +0200

    quick fix #8

--- collaborative_filtering/prediction/main_prediction.py
+++ collaborative_filtering/prediction/main_prediction.py
@@ -7,7 +7,12 @@ rating_matrix = np.genfromtxt("../../dataset/R.csv", delimiter=",")
 is_rated_matrix = np.genfromtxt("../../dataset/Y.csv", delimiter=",").astype(bool)
 data = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
 
-print(predicition_cosine_similarity(0, 0, data))
+
+print(predicition_cosine_similarity(750, 0, data))
+print(predicition_cosine_similarity(1341, 0, data))
+print(predicition_cosine_similarity(1357, 0, data))
+print(predicition_cosine_similarity(1388, 0, data))
+print(predicition_cosine_similarity(1661, 0, data))
 top_n_list = get_top_n_list(10, 0, data)
 for it in top_n_list:
     print("--------------------")
