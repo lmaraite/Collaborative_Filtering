a0e71b3a33d0228c9d5ecd41b9369c5e800dc504
lmaraite <leon.maraite@gmail.com>
2020-05-23 17:16:16 +0200

    fix #9

--- collaborative_filtering/prediction/main_prediction.py
+++ collaborative_filtering/prediction/main_prediction.py
@@ -7,10 +7,11 @@ rating_matrix = np.genfromtxt("../../dataset/R.csv", delimiter=",")
 is_rated_matrix = np.genfromtxt("../../dataset/Y.csv", delimiter=",").astype(bool)
 data = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
 
-
+print(predicition_cosine_similarity(0, 0, data))
 top_n_list = get_top_n_list(10, 0, data)
 for it in top_n_list:
     print("--------------------")
     print("movie_id:          "+str(it[0]))
     print("rating_prediction: "+str(it[1]))
     print("--------------------")
+
