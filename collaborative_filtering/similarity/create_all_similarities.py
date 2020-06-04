a5f831a5b7a08ce3fdeaf98a5632d5ebd7bc8498
dkettner <kettner_dennis@web.de>
2020-06-04 15:39:37 +0200

    Add: Two new matrices get created.
    In order to save computation time during prediction the numbers of the individual co-ratings get saved in an additional matrix. This is done for both user-based and item-based.
    These matrices are intended to help with weighting the similarities.

--- collaborative_filtering/similarity/create_all_similarities.py
+++ collaborative_filtering/similarity/create_all_similarities.py
@@ -4,7 +4,7 @@ site.addsitedir(os.path.join(os.path.abspath(sys.path[0]), ".."))
 import numpy as np
 import timeit
 import similarity
-from similarity import create_similarity_matrix
+from similarity import create_similarity_matrix, create_number_of_co_ratings_matrix
 from input.filesystem import read_ratings_matrix, read_is_rated_matrix
 
 
@@ -22,11 +22,22 @@ algorithms = [similarity.COSINE, similarity.ADJUSTED_COSINE, similarity.PEARSON]
 approaches = [similarity.ITEM_BASED, similarity.USER_BASED]
 
 for approach in approaches:
+    local_start_time = timeit.default_timer()
+
+    number_of_co_ratings_matrix = create_number_of_co_ratings_matrix(approach, item_based_ratings, is_rated)
+    np.savetxt("../output/" + approach + "_" + "number_of_co_ratings_matrix.csv",
+               number_of_co_ratings_matrix, delimiter=",", fmt="%s")
+
+    local_elapsed_time = timeit.default_timer() - local_start_time
+    print("The program has taken " + str(local_elapsed_time) + " seconds to create and save the "
+          + approach + "_" + "number_of_co_ratings_matrix.")
+
     for algorithm in algorithms:
         local_start_time = timeit.default_timer()
 
         similarity_matrix = create_similarity_matrix(approach, algorithm, item_based_ratings, is_rated)
-        np.savetxt("../output/" + approach + "_" + algorithm + "_similarity_matrix.csv", similarity_matrix, delimiter=",", fmt="%s")
+        np.savetxt("../output/" + approach + "_" + algorithm + "_similarity_matrix.csv",
+                   similarity_matrix, delimiter=",", fmt="%s")
 
         local_elapsed_time = timeit.default_timer() - local_start_time
         print("The program has taken " + str(local_elapsed_time) + " seconds to create and save the "
