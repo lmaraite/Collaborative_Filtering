Fix: Total error is now the average error of individual test runs error

test/collaborative_filtering/evaluation/accuracy_test.py
@@ -171,8 +171,8 @@ class AccuracyEvaluationTest(unittest.TestCase):
         return self.prediction_return
 
     def callback_error_measurement_mock(self, predictions, ratings):
-        assert_are_same_matrices(predictions, np.array([self.prediction_return[0]]))
-        assert_are_same_matrices(ratings, np.array([self.ratings_matrix[0, 2]]))
+        assert predictions == [self.prediction_return[0]]
+        assert ratings == [self.ratings_matrix[0, 2]]
 
         return self.error_return
 
@@ -219,3 +219,68 @@ class AccuracyEvaluationTest(unittest.TestCase):
         error = ac.run_accuracy_evaluation(eval_props)
         #then
         assert error == self.error_return
+
+@pytest.fixture
+def mock_empty(mocker):
+    mocker.patch("evaluation.selection.select_indices_with_hold_out")
+    mocker.patch("evaluation.selection.keep_elements_by_index")
+    mocker.patch("similarity.create_similarity_matrix")
+    mocker.patch("prediction.prediction.predicition_cosine_similarity")
+    mocker.patch("evaluation.accuracy.root_mean_squared_error")
+
+def cross_validation_mock_callback(shape, is_rated_matrix, train_size):
+    yield iter([(1, 0), (2, 0)]), iter([(0, 0)])
+    yield iter([(0, 0)]), iter([(1, 0), (2, 0)])
+
+def prediction_function_mock_callback(key_id, element_id, dataset):
+    assert key_id in [0, 1, 2]
+    assert element_id == 0
+
+    if key_id == 0:
+        return 2, -1
+    if key_id == 1:
+        return 5, -1
+    if key_id == 2:
+        return 1, -1
+
+def error_function_mock_callback(predictions, actual_ratings):
+    assert predictions in [[2], [5, 1]]
+    assert actual_ratings in [[4], [1, 4]]
+
+    if predictions == [2] and actual_ratings == [4]:
+        return 2
+    elif predictions == [5, 1] and actual_ratings == [1, 4]:
+        return 3.5
+
+def test_run_accuracy_evaluation_error_with_multiple_splits(mock_empty, mocker):
+    #given
+    select_strategy_mock = mocker.patch("evaluation.selection.select_indices_with_cross_validation")
+    select_strategy_mock.side_effect = cross_validation_mock_callback
+
+    prediction_function_mock = mocker.patch("prediction.prediction.predicition_cosine_similarity")
+    prediction_function_mock.side_effect = prediction_function_mock_callback
+
+    error_function_mock = mocker.patch("evaluation.accuracy.root_mean_squared_error")
+    error_function_mock.side_effect = error_function_mock_callback
+
+    ratings_matrix = np.array([
+        [4],
+        [1],
+        [4]
+    ])
+    is_rated_matrix = ratings_matrix != 0
+
+    eval_props = SinglePredictionAccuracyEvaluationProperties(
+        ratings_matrix,
+        is_rated_matrix,
+        COSINE,
+        selection.select_indices_with_cross_validation,
+        0.8,
+        ac.root_mean_squared_error,
+        prediction.predicition_cosine_similarity
+    )
+
+    #when
+    error = ac.run_accuracy_evaluation(eval_props)
+    #then
+    assert error == 2.75