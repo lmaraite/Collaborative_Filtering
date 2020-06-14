49c41407a05cf2ea513300758baf4871839ad9a3
Codor77 <c.o.d.o.r.axp@gmail.com>
2020-06-14 16:43:54 +0200

    Add: user-based top-n

--- collaborative_filtering/prediction/main.py
+++ collaborative_filtering/prediction/main.py
@@ -7,16 +7,20 @@ import sys
 similarity_matrix = np.genfromtxt("../../output/item_based_pearson_similarity_matrix.csv", delimiter=",")
 rating_matrix = np.genfromtxt("../../dataset/R.csv", delimiter=",")
 is_rated_matrix = np.genfromtxt("../../dataset/Y.csv", delimiter=",").astype(bool)
-data = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
+data_item = dataset(similarity_matrix, rating_matrix, is_rated_matrix)
 
-top_n_list_item = get_top_n_list(10, 0, data, "pearson", "item")
+
 
 similarity_matrix = np.genfromtxt("../../output/user_based_pearson_similarity_matrix.csv", delimiter=",")
 rating_matrix = np.genfromtxt("../../dataset/R.csv", delimiter=",")
 is_rated_matrix = np.genfromtxt("../../dataset/Y.csv", delimiter=",").astype(bool)
-data = dataset(similarity_matrix, rating_matrix.transpose(), is_rated_matrix.transpose())
+data_user = dataset(similarity_matrix, rating_matrix.transpose(), is_rated_matrix.transpose())
 
-top_n_list_user = get_top_n_list(10, 0, data, "pearson", "user")
+user_id = 155
+n = 10
+algorithm = "pearson"
+top_n_list_item = get_top_n_list(n, user_id, data_item, algorithm, "item")
+top_n_list_user = get_top_n_list(n, user_id, data_user, algorithm, "user")
 
 for it in range(0, len(top_n_list_user)):
     print("--------------------")
